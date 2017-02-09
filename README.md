fa9383e23c941f23440e76f590e013a5640bd5f2
DanielSrb <srb.dan@gmail.com>
2017-02-09 14:06:39 +0100

    initial release

--- README.md
+++ README.md
@@ -5,12 +5,25 @@
 With node.js installed run
 
 ```shell
+sudo npm i gulp-cli -g
+```
+
+```shell
 npm i
 ```
 
+## Development
+
+Enjoy automatic refreshing of everything by running.
+
+```shell
+gulp develop
+```
+
+
 ## Deployment
 
-With installed surge client (`npm i surge --global`) run
+With installed surge client (`npm i surge -g`) run
 
 ```shell
 gulp deploy
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,53 @@
 var gulp = require('gulp');
+
+var clean = require('gulp-clean');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var browserSync = require('browser-sync').create();
 var surge = require('gulp-surge');
 
-gulp.task('deploy', [], function(){
+
+
+gulp.task('cssclean', function(){
+	return gulp.src('dist/css', { read: false }).pipe(clean());
+});
+
+
+
+gulp.task('csscompile', ['cssclean'], function(){
+	return gulp
+		.src('src/sass/index.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sourcemaps.init())
+		.pipe(postcss([autoprefixer()]))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('dist'))
+		.pipe(browserSync.stream());
+});
+
+
+
+gulp.task('develop', ['csscompile'], function(){
+	browserSync.init({
+		server: {
+			baseDir: './'
+		},
+		port:3030,
+		files: ['*.html', 'dist/**/*.css', 'dist/**/*.js']
+	});
+
+	gulp.watch('src/sass/index.scss', ['csscompile']);
+});
+
+
+
+gulp.task('deploy', ['csscompile'], function(){
 	return surge({
 		project: './',
 		domain: 'webkurz.pyladies.cz'
 	})
 });
+
+gulp.task('default', ['develop']);
