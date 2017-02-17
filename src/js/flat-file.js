54b1f0630409a931695f12f958cdc23ce3990fc5
DanielSrb <srb.dan@gmail.com>
2017-02-18 00:10:59 +0100

    nove menu: - nahrazuje .tabs - ve vsech lekcich je ted stejne menu - automaticky se v nem oznacuje aktualni stranka - je umisteno v jednom souboru /lekce/_menu.html (nacita se pres xhr) - polozka menu se na produkci skryje, pokud  ma `.menu-label` pred danym odkazem v sobe `.tag.is-primary` - polozka menu se na produkci stane neklikatelnou, pokud ma `a` v sobe `.tag.is-primary` - vsechny HTML soubory lekci jsou nyni stejne protoze      - .md soubor se nacita automaticky podle nazvu souboru .html      - titulek se generuje z nazvu polozky v menu      - odkaz na dalsi se generuje taky automaticky (pokud nasledujici `.menu-label` nema  v sobe `.tag.is-primary`) - pozn. na homepage je rozcestnik stale rucne

--- src/js/flat-file.js
+++ src/js/flat-file.js
@@ -4,12 +4,73 @@
 
 $(document).ready(
 	function(){
+
+		var $main = $('[data-markdown]'); // main content element
+
+		/*
+		 * Navigation
+		 */
+
+		// Load menu
+		$('.c-menu').load('/lekce/_menu.html');
+
+		var $nextChapter = '';
+
+		// set current menu item
+		$(document).ajaxComplete(function(event, xhr, settings){
+			if (settings.url === '/lekce/_menu.html'){
+
+				var $menuLinks = $('.menu a');
+
+				$menuLinks.each(function(i){
+					if ($(this).attr('href') == window.location.pathname){
+						$(this).addClass('is-active');
+						// generate title
+						$('title').text($(this).text() + ': ' + $('title').text());
+
+						// set next chapter
+						if (i + 1 < $menuLinks.length){
+							if (!$menuLinks.eq(i + 1).closest('.menu-list').prev('.menu-label').find('.tag').hasClass('is-primary')){
+								$nextChapter = $menuLinks.eq(i + 1);
+							}
+						}
+					}
+
+					if (window.location.hostname != 'localhost'){
+						if ($(this).closest('.menu-list').prev('.menu-label').find('.tag').hasClass('is-primary')){
+							$(this).hide();
+						} else if ($(this).find('.tag').hasClass('is-primary')){
+							$(this).addClass('is-disabled is-unselectable');
+						}
+					}
+				});
+
+				// Generate next link
+				if ($nextChapter){
+					var nextLink = '<div class="tile"><div class="tile is-parent"><a href="' +
+						$nextChapter.attr('href') +
+						'" class="tile is-child notification is-info has-text-right">další kapitola <em>' +
+						$nextChapter.text() +
+						'</em></a></div></div>';
+					$main.after(nextLink);
+				}
+			}
+		});
+
+
+		/*
+		 * Markdown rendering
+		 */
+
+		// custom renderer
 		var renderer = new marked.Renderer();
 
+		// no ids in headings
 		renderer.heading = function(text, level){
 			return `<h${level}>${text}</h${level}>`;
 		};
 
+		// set options
 		marked.setOptions({
 			renderer: renderer,
 			gfm: true,
@@ -19,77 +80,55 @@ $(document).ready(
 			}
 		});
 
-		// console.info(window.location.pathname.split('/').slice(-1)[0]+'.md');
-
-		$('[data-markdown]').each(function(){
-			var $element = $(this);
-
-			$
-				.get($element.data('markdown'), function(data){
-
-					var result = '';
-
-					jQuery.each(data.split(/\r?\n---\s*\r?\n/g), function(key, value){
-
-						var subcontent = value.split(/\r?\n----\s*\r?\n/g);
-
-						jQuery.each(subcontent, function(subkey, subvalue){
-							var className = '';
-
-							if (subvalue.match(/^<!-- .slide: data-state="(.+?)" -->$/m)) className = subvalue.match(/^<!-- .slide: data-state="(.+?)" -->$/m);
-
-							if (subkey === 0){
-								result += '<div class="tile"><div class="tile is-parent ';
-								if (subcontent.length > 1) result += 'is-3';
-								result += '"><div class="tile is-child notification ' + className[1] + '"><div class="content">';
-								result += marked(subvalue);
-								result += '</div></div></div>';
-
-								if (subcontent.length > 1) result += '<div class="tile is-vertical">';
-
-							} else {
-								result += '<div class="tile is-parent"><div class="tile is-child notification ' + className[1] + '"><div class="content">';
-								result += marked(subvalue);
-								result += '</div></div></div>';
-							}
-
-						});
-
-						if (subcontent.length > 1) result += '</div>';
-
-						result += '</div>';
-					});
-
-					$element.html(result);
-
-				})
-				.fail(function(){
-					$element.html('<p>Could not find: <code>' + $element.data('markdown') + '</code></p>');
-				});
-		});
 
 		/*
-		function createToc(){
-			$('.toc').toc({
-				'selectors': 'h1,h2,h3', //elements to use as headings
-				'container': '.tile', //element to find all selectors in
-				'smoothScrolling': false, //enable or disable smooth scrolling on click
-				'prefix': 'toc', //prefix for anchor tags and class names
-				'onHighlight': function(el){
-				}, //called when a new section is highlighted
-				'highlightOnScroll': true, //add class to heading that is currently in focus
-				'highlightOffset': 100, //offset to trigger the next headline
-				'anchorName': function(i, heading, prefix){ //custom function for anchor name
-					return prefix + i;
-				},
-				'itemClass': function(i, heading, $heading, prefix){ // custom function for item class
-					return $heading[0].tagName.toLowerCase();
-				}
+		 * Load and render data
+		 */
+		$.get(window.location.pathname.split('/').slice(-1)[0] + '.md', function(data){
+
+			var result = '';
+
+			jQuery.each(data.split(/\r?\n---\s*\r?\n/g), function(key, value){
+
+				var subcontent = value.split(/\r?\n----\s*\r?\n/g);
+
+				jQuery.each(subcontent, function(subkey, subvalue){
+					var className = '';
+
+					if (subvalue.match(/^<!-- .slide: data-state="(.+?)" -->$/m)) className = subvalue.match(/^<!-- .slide: data-state="(.+?)" -->$/m);
+
+					if (subkey === 0){
+						result += '<div class="tile"><div class="tile is-parent ';
+						if (subcontent.length > 1) result += 'is-3';
+						result += '"><div class="tile is-child notification ' + className[1] + '"><div class="content">';
+						result += marked(subvalue);
+						result += '</div></div></div>';
+
+						if (subcontent.length > 1) result += '<div class="tile is-vertical">';
+
+					} else {
+						result += '<div class="tile is-parent"><div class="tile is-child notification ' + className[1] + '"><div class="content">';
+						result += marked(subvalue);
+						result += '</div></div></div>';
+					}
+
+				});
+
+				if (subcontent.length > 1) result += '</div>';
+
+				result += '</div>';
 			});
-		}
 
-		setTimeout(createToc, 3000);
-		*/
+			$main.html(result);
 
+		})
+			.fail(function(){
+				$main
+					.html(
+						'<div class="tile"><div class="tile is-parent"><div class="tile is-child notification is-danger"><div class="content"> '
+						+ '<div class="tile"><p class="title is-1">Nepovedlo se načíst data, zkus obnovit stránku.</p></div>'
+						+ '</div></div></div></div>'
+					);
+			});
 	}
 );
